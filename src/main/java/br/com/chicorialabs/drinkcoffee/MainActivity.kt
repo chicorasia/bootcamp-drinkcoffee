ef889610c958f7d8ab741c77553218877a7e84fa
chicorasia <chicorasia@gmail.com>
2021-04-06 15:28:11 -0300

    Implementado salvamento em DataStore a partir da MainActivity

--- src/main/java/br/com/chicorialabs/drinkcoffee/MainActivity.kt
+++ src/main/java/br/com/chicorialabs/drinkcoffee/MainActivity.kt
@@ -1,25 +1,28 @@
 package br.com.chicorialabs.drinkcoffee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import br.com.chicorialabs.drinkcoffee.databinding.ActivityMainBinding
 import br.com.chicorialabs.drinkcoffee.utils.PreferencesUtils
+import br.com.chicorialabs.drinkcoffee.utils.PreferencesUtils.Companion.KEY_COFFEE_COUNT
 import br.com.chicorialabs.drinkcoffee.viewmodel.DrinkCoffeeViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
+//    TODO 002: criar um objeto DataStore como atributo da MainActivity*
+
+lateinit var dataStore: DataStore<Preferences>
 
 class MainActivity : AppCompatActivity() {
-
-
-//    TODO 002: criar um objeto DataStore como atributo da MainActivity
-//    TODO 003: inicializar o dataStore, com o nome desejado
-//    TODO 004: criar uma função para gravar uma preferência, recebe uma chave e um valor
-//    TODO 005: criar uma função para ler uma preferência, recebe somente uma chave
-//    TODO 006: chamar o métodos save() no OnClickListener()
-//    TODO 007: chamar o método save() no OnLongClickListener(), passando 0 como valor
-//    TODO 008: chamar o método read() no initQuantity() para iniciar o contador com o valor gravado
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mViewModel: DrinkCoffeeViewModel
@@ -38,20 +41,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+//    TODO 003: inicializar o dataStore, com o nome desejado
+
+        dataStore = createDataStore(name = "coffee_counter")
+
         mViewModel = ViewModelProvider(this).get(DrinkCoffeeViewModel::class.java)
 
         initQuantity()
         initObserver()
 
-
+//    TODO 006: chamar o métodos save() no OnClickListener()
         cupImageview.setOnClickListener {
+//            PreferencesUtils(this).saveCoffeeCount(mViewModel.coffeeCounter.value)
             mViewModel.incrementCounter()
-            PreferencesUtils(this).saveCoffeeCount(mViewModel.coffeeCounter.value)
-
+            lifecycleScope.launch {
+                val counter = mViewModel.coffeeCounter.value
+                save(KEY_COFFEE_COUNT, counter ?: 0)
+            }
         }
 
+//    TODO 007: chamar o método save() no OnLongClickListener(), passando 0 como valor
         cupImageview.setOnLongClickListener {
-            PreferencesUtils(this).saveCoffeeCount(0)
+//            PreferencesUtils(this).saveCoffeeCount(0)
+            lifecycleScope.launch {
+                save(KEY_COFFEE_COUNT, 0)
+            }
             mViewModel.resetCounter()
         }
     }
@@ -62,10 +76,32 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+//    TODO 008: chamar o método read() no initQuantity() para iniciar o contador com o valor gravado
     private fun initQuantity() {
-        mViewModel.setCoffeCounterTo(PreferencesUtils(this).loadCoffeeCount())
+//        mViewModel.setCoffeCounterTo(PreferencesUtils(this).loadCoffeeCount())
+        lifecycleScope.launch {
+            mViewModel.setCoffeCounterTo(read(KEY_COFFEE_COUNT) ?: 0)
+        }
         quantityTxt.text = mViewModel.coffeeCounter.value.toString()
 
     }
+
+//    TODO 004: criar uma função para gravar uma preferência, recebe uma chave e um valor
+
+    suspend fun save(key: String, value: Int) {
+        val dataStoreKey = preferencesKey<Int>(key)
+        dataStore.edit { coffeeCounter ->
+            coffeeCounter[dataStoreKey] = value
+        }
+    }
+
+//    TODO 005: criar uma função para ler uma preferência, recebe somente uma chave
+    suspend fun read(key: String) : Int? {
+        val dataStoreKey = preferencesKey<Int>(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
+
+
 
 }
--- src/main/java/br/com/chicorialabs/drinkcoffee/utils/PreferencesUtils.kt
+++ src/main/java/br/com/chicorialabs/drinkcoffee/utils/PreferencesUtils.kt
@@ -4,25 +4,25 @@ import android.content.Context
 
 class PreferencesUtils(context: Context) {
 
-//    TODO 001A: comentar esse campo e métodos e ver onde o código quebra
-    val prefs = context.getSharedPreferences(KEY_COFFEE_COUNT, Context.MODE_PRIVATE)
+//    TODO 001A: comentar esse campo e métodos e ver onde o código quebra *
+//    val prefs = context.getSharedPreferences(KEY_COFFEE_COUNT, Context.MODE_PRIVATE)
 
     companion object{
         const val KEY_COFFEE_COUNT = "br.com.chicorialabs.drinkcoffee.COFFEE_COUNTER"
         const val DEFAULT_VALUE = 0
 
     }
-
-    fun loadCoffeeCount() : Int {
-        return prefs.getInt(KEY_COFFEE_COUNT, DEFAULT_VALUE)
-    }
-
-    fun saveCoffeeCount(coffeCount: Int?) {
-        with (prefs.edit()) {
-            putInt(KEY_COFFEE_COUNT, coffeCount ?: 0)
-            commit()
-        }
-    }
+//
+//    fun loadCoffeeCount() : Int {
+//        return prefs.getInt(KEY_COFFEE_COUNT, DEFAULT_VALUE)
+//    }
+//
+//    fun saveCoffeeCount(coffeCount: Int?) {
+//        with (prefs.edit()) {
+//            putInt(KEY_COFFEE_COUNT, coffeCount ?: 0)
+//            commit()
+//        }
+//    }
 
 
 }
